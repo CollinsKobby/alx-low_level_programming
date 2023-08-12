00b495956b37b329397de280febd47535d5c2986
CollinsKobby <collinsnukporti888@gmail.com>
2023-08-11 21:29:43 -0700

    Variables if else while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i ; i <= 9 ; i++)
+		printf(i);
+	printf("\n");
+	return (0);
+}
