1e0eab395dfd8078f6d810ca1e89c0674294f096
CollinsKobby <collinsnukporti888@gmail.com>
2023-08-17 17:24:00 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers
+ *
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int z;
+
+	if ((z >= 0) && (z <= 9))
+	{
+		if ((z == 2) && (z == 4))
+		{
+
+		}
+		else
+		_putchar(z);
+	}
+	_putchar('\n');
+}
