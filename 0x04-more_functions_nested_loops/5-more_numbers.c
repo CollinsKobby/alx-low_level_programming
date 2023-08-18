c46a52c0f7e6a59b406f4591988da6801e6fb2c0
CollinsKobby <collinsnukporti888@gmail.com>
2023-08-17 17:29:23 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * more_numbers - prints numbers
+ *
+ * Return: 0
+ */
+
+
+void more_numbers(void)
+{
+	int z;
+
+	
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ *
+ * Return: 0
+ */
+
+
+void print_line(int n)
+{
+	if (n >= 1)
+	{
+		_putchar(1);
+	}
+	else 
+	{
+		_putchar('\n');
+	}
+}
