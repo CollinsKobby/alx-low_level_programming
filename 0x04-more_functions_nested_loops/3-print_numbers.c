011244e7d927259c08de081375dba86696332e7c
CollinsKobby <collinsnukporti888@gmail.com>
2023-08-17 17:07:31 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	if (n <= 0 && n >= 9)
+	{
+		_putchar(n)
+	}
+	_putchar('\n')
+
+	return (0);
+}
