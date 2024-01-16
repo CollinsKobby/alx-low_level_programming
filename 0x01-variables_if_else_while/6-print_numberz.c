17172b3163bbe02648b0071c6119a6d1e018c142
CollinsKobby <collinsnukporti888@gmail.com>
2024-01-15 18:28:43 -0800

    Changes

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - This the main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
