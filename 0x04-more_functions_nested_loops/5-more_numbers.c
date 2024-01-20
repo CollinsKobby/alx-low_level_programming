b3fe084d788a585d39af9a6799c59bbd4210d67f
CollinsKobby <collinsnukporti888@gmail.com>
2024-01-19 19:00:09 -0800

    Print line

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * more_numbers - prints numbers
  *
@@ -9,7 +9,13 @@
 
 void more_numbers(void)
 {
-	int z;
+	int z = 0;
+	int n = 01234567891011121314;
 
-	
+	while (z < 11)
+	{
+		_putchar(n);
+		z++;
+	}
+	_putchar ('\n');
 }
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,12 +10,16 @@
 
 void print_line(int n)
 {
-	if (n >= 1)
-	{
-		_putchar(1);
-	}
-	else 
+	int i;
+
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
+	else
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
 }
