540418f7973f19291bba382efc1aa68ac497491f
CollinsKobby <collinsnukporti888@gmail.com>
2024-01-30 08:24:22 -0800

    Arrays and pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - counts the number of strings
+ *
+ * @s: character pointer
+ *
+ * Return: always 0
+ */
+
+int _strlen(char *s)
+{
+	strlen(s);
+
+	return (0);
+}
+
