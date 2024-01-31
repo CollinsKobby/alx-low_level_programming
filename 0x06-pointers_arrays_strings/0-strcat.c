b7869603ca4e6fe3d0d2348b24e91d826b3703e8
CollinsKobby <collinsnukporti888@gmail.com>
2024-01-31 04:33:18 -0800

    More pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concantenates two strings
+ *
+ * @dest: first member
+ * @src: second member
+ *
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *strcat(char *dest, const char *src);
+
+	/*return (dest)*/
+}
+
