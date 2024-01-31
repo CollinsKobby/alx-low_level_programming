d99795276c91f44017cda90e55708c1c4373a5b8
CollinsKobby <collinsnukporti888@gmail.com>
2024-01-31 05:57:48 -0800

    More pointers 4

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: first member
+ * @s2: second member
+ *
+ * Return: strncmp()
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int strcmp(const char *s1, const char *s2);
+
+	return (strcmp(s1, s2));
+}
