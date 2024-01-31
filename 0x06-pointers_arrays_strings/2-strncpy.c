757f5dfe6cff410f40c866381784720946456c19
CollinsKobby <collinsnukporti888@gmail.com>
2024-01-31 05:41:37 -0800

    More pointers 3

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.>
+#include "main.h"
+
+/**
+ * _strncpy - copies strings
+ *
+ * @dest: first member
+ * @src: second member
+ * @n: third member
+ *
+ * Return: strncpy()
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *strncpy(char *dest, const char *src, size_t n);
+
+	return (strncpy(dest, src, n));
+}
+
