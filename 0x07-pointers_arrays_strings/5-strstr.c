9e46a1d739df1804d808de360f2689139d582a9a
CollinsKobby <collinsnukporti888@gmail.com>
2024-02-02 19:39:47 -0800

    Arrays 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strstr - finds first occurence
+ *
+ * @haystack: first member
+ * @needle: second member
+ *
+ * Return: strstr()
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *strstr(const char *haystack, const char *needle);
+
+	return (strstr(haystack, needle));
+}
+
