b85a831b0a14296e91a11ff37fe75061ebb27111
CollinsKobby <collinsnukporti888@gmail.com>
2024-02-02 19:09:31 -0800

    Arrays 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strpbrk - locates first occurence in the string
+ *
+ * @s: first member
+ * @accept: second member
+ *
+ * Return: strpbrk()
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *strpbrk(const char *s, const char *accept);
+
+	return (strpbrk(s, accept));
+}
