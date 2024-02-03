ab901fce84e353f67245aa0a8835e80af9a2b5c5
CollinsKobby <collinsnukporti888@gmail.com>
2024-02-02 18:56:15 -0800

    More arrays 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strspn - calculates length 
+ *
+ * @s: first member
+ * @accept: second member
+ *
+ * Return: strspn()
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	size_t strspn(const char *s, const char *accept);
+
+	return (strspn(s, accept));
+}
+
