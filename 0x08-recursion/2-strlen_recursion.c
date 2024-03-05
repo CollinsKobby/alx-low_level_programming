e5306fd7b7f34d0470868bfccb77cbb40377a1b7
CollinsKobby <collinsnukporti888@gmail.com>
2024-03-04 18:12:48 -0800

    Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - counting strings using recursion
+ * @s: string pointer
+ *
+ * Return: strlen
+ */
+
+int _strlen_recursion(char *s)
+{
+	size_t strlen(const char *s);
+	return (strlen(s));
+}
