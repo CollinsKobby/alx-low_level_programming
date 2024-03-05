804d37732b9eb9f435a0793f5cc25d2f52dfdbd4
CollinsKobby <collinsnukporti888@gmail.com>
2024-03-04 16:39:11 -0800

    Changes

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - recursive function of puts
+ *
+ * @s: charcater pointer
+ * Return: puts(s)
+ */
+
+void _puts_recursion(char *s)
+{
+	int puts(const char *s);
+
+	return (puts(s));
+}
