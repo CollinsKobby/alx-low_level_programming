3a33292aada689beae28b6b598925c462a3fe0bd
CollinsKobby <collinsnukporti888@gmail.com>
2024-03-05 06:37:07 -0800

    Power recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - power of a number
+ * @x: number
+ * @y: power
+ *
+ * Return: pow()
+ */
+
+int _pow_recursion(int x, int y)
+{
+	double pow(double x, double y);
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	return (pow(x, y));
+}
