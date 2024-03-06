fc621c448bc3af57b1dc497d6cd1b44cac2e44cb
CollinsKobby <collinsnukporti888@gmail.com>
2024-03-06 15:02:56 -0800

    Argc argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: count arguments
+ * @argv: array of argument
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		if (*argv[i] > 1)
+		{
+			sum += atoi(argv[i]);
+		}
+		else if (argc == 1)
+		{
+			printf("0\n");
+		}
+	}
+	printf("%d\n", sum);
+
+	return(0);
+}
