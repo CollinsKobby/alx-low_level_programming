44fa3439b95004472ace74ce4ef78168e2228512
CollinsKobby <collinsnukporti888@gmail.com>
2024-03-06 06:55:36 -0800

    Agrc agrv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,15 +3,16 @@
 
 /**
  * main - the main function
- * @argc: counts number of command line arguments
- * @argv[]: array of the command line argument
+ * @agrc: counts number of command line arguments
+ * @agrv: array of the command line argument
  *
  * Return: always 0
  */
 
 int main(int agrc, char *agrv[])
 {
-	printf("%s", argv[0]);
+	(void) agrc;
+	printf("%s\n", agrv[0]);
 
 	return (0);
 }
