d6e95897461ebb0c6dfa8e6ae02c8d55c75248b6
CollinsKobby <collinsnukporti888@gmail.com>
2024-03-06 06:40:35 -0800

    Agrc Agrv[]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - the main function
+ * @argc: counts number of command line arguments
+ * @argv[]: array of the command line argument
+ *
+ * Return: always 0
+ */
+
+int main(int agrc, char *agrv[])
+{
+	_putchar(argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+Argc - for counting the number of command line arguments used when executing the program
+Argv[] - An array of the command line arguments used when executing the program  
