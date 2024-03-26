76ed7f3d9239cb5fd13a3052d277b005c57c679a
CollinsKobby <collinsnukporti888@gmail.com>
2024-03-26 06:49:57 -0700

    God

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - dog's info
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: about the name and age of the dog as well the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
