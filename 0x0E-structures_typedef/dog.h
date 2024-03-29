2277d2b69cf08b80c2000ae167a3e3ca6944cef6
CollinsKobby <collinsnukporti888@gmail.com>
2024-03-29 16:00:34 -0700

    Init dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,3 +17,11 @@ typedef struct dog
 	char *owner;
 } dog_t;
 #endif
+#ifndef _FUNCTIONS_H_
+#define _FUNCTIONS_H
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
